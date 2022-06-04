64ecd4576d09d61edcd92819b89afdcd987137cb
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-05 01:39:21 +0530

    Data From CSV file

--- data/model.py
+++ data/model.py
@@ -1,22 +1,32 @@
+import csv
 
-ITEMS = [
+# ITEMS = [
 
 
-    {'id' : 1200, 'name': "Coffee", 'BarcodeID' : 'B60khaB12#' , 'Price' : '$100'},
-    {'id' : 1201, 'name': "Chips", 'BarcodeID' : 'B60khaB13#' , 'Price' : '$200'},
-    {'id' : 1202, 'name': "Choclate", 'BarcodeID' : 'B60khaB15#' , 'Price' : '$300'},
-    {'id' : 1203, 'name': "Iphone", 'BarcodeID' : 'B60khaB14#' , 'Price' : '$1000'},
+#     {'id' : 1200, 'name': "Coffee", 'BarcodeID' : 'B60khaB12#' , 'Price' : '$100'},
+#     {'id' : 1201, 'name': "Chips", 'BarcodeID' : 'B60khaB13#' , 'Price' : '$200'},
+#     {'id' : 1202, 'name': "Choclate", 'BarcodeID' : 'B60khaB15#' , 'Price' : '$300'},
+#     {'id' : 1203, 'name': "Iphone", 'BarcodeID' : 'B60khaB14#' , 'Price' : '$1000'},
 
-]
+# ]
 
 # for items in ITEMS:
 
 #     for k,v in items.items():
 
-#         print(f"The value of {k} is {v}")
+#         print(f"{v}",end=",")
     
 #     print("\n")
 
+ITEMS = []
+
+with open('/Users/anton/Desktop/dev/flask-tutorials/data/csv_data/items.csv','r') as file:
+
+    data = csv.DictReader(file)
+
+    for row in data:
+        
+        ITEMS.append(dict(row))
 
 
 
