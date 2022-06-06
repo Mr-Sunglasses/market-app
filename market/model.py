d1308011d671634949220c1b3f96084dc3918808
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-07 00:07:41 +0530

    Added RelationShips

--- market/model.py
+++ market/model.py
@@ -6,7 +6,9 @@ class Users(db.Model):
     username = db.Column(db.String(length=20), nullable=False, unique=True)
     emailID = db.Column(db.String(length=250), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=255), nullable=False)
-    budget = db.column(db.Integer(), nullable=False, default=1000)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    item = db.relationship('Item', backref='owned_user', lazy=True)  # This Defines the RelationShip with the Item
+    # Model That which Item is Associated to The User
 
 
 class Item(db.Model):
@@ -15,6 +17,8 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=2555), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('users.id'))
 
     def __repr__(self) -> str:
         return f'Items {self.id} {self.name}'
+
