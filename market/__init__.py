aa33f0932862bc6f400f156472033edefc872ba8
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-07 01:32:35 +0530

    Rename key

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from KEY import KEY
+from KEY import KEY_TO_ACCESS
 
 APP = Flask(__name__)
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-APP.config['SECRET_KEY'] = KEY
+APP.config['SECRET_KEY'] = KEY_TO_ACCESS
 
 db = SQLAlchemy(APP)
 
