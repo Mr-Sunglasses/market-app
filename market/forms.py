c1eecaeeb8ece6aa2b33ab4a20c57f179cf09346
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-07 20:14:45 +0530

    Added expection handling in forms

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, DataRequired, Email
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label="Username")
-    emailID = StringField(label="Email")
-    password1 = PasswordField(label="Password")
-    confirm_password1 = PasswordField(label="Confirm password")
+    username = StringField(label="Username", validators=[Length(min=4,max=40), DataRequired()])
+    emailID = StringField(label="Email", validators=[Email(), DataRequired()])
+    password1 = PasswordField(label="Password", validators=[Length(min=6), DataRequired()])
+    confirm_password1 = PasswordField(label="Confirm password", validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label="Create Account")
--- market/routes.py
+++ market/routes.py
@@ -2,10 +2,9 @@ from crypt import methods
 from flask import Flask, render_template, redirect, url_for
 from market import APP
 from market import forms
-from market.model import Item,Users
+from market.model import Item, Users
 from market.forms import RegistrationForm
 from market import db
-
 
 
 @APP.route('/')
@@ -26,18 +25,21 @@ def login():
     return f"<h1>Login</h1>"
 
 
-@APP.route('/register', methods= ['GET', 'POST'])
+@APP.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-
-        user_to_create = Users(username = form.username.data, emailID = form.emailID.data, password_hash = form.password1.data)
+        user_to_create = Users(username=form.username.data, emailID=form.emailID.data,
+                               password_hash=form.password1.data)
 
         db.session.add(user_to_create)
         db.session.commit()
 
         return redirect(url_for('marketpage'))
-    
 
-    return render_template('register.html',forms=form)
+    if form.errors != {}:  # This shows that If there is Errors in the form which is stored in the form of dictionary
+        for error_all in form.errors.values():
+            print(f"The Error is {error_all}")
+
+    return render_template('register.html', forms=form)
