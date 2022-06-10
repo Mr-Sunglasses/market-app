9aa048e5bf762a0a888e357af02cd03255bee94a
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-10 23:47:22 +0530

    Documentation

--- market/model.py
+++ market/model.py
@@ -3,6 +3,7 @@ from market import bcrypt
 from flask_login import UserMixin
 
 
+# User for Login_Required
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
