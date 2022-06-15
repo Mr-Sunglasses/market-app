e8073c64090581cc2461bbe00ba2e12dc2d0c709
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-15 18:39:22 +0530

    changes

--- market/model.py
+++ market/model.py
@@ -8,6 +8,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
+
 # Class For Database Model of Users
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
@@ -16,6 +17,7 @@ class Users(db.Model, UserMixin):
     password_hash = db.Column(db.String(length=255), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     item = db.relationship('Item', backref='owned_user', lazy=True)  # This Defines the RelationShip with the Item
+
     # Model That which Item is Associated to The User
 
     # Used to Prettify the budget and add a ',' like before - 1000 , After 1,000
@@ -38,6 +40,7 @@ class Users(db.Model, UserMixin):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
 
 # Class for Item Model in Database
 class Item(db.Model):
